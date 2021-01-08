28221609346d6388b7300d4ef0d1f54cfb01c99e
VGUDUR3 <vishal.gudur@jobsnprofiles.com>
2021-01-07 17:20:36 -0700

    wtau

---
+++ wp-content/themes/TheFox/js/refresh_bp0606.js
@@ -0,0 +1,18 @@
+/** refresh **/
+
+jQuery.noConflict();
+
+var $ = jQuery;
+
+
+function watchblog() {
+
+$(".masonry_ctn").isotope({
+  // options
+  itemSelector : '.ajax_post',
+  layoutMode : 'masonry'
+});
+
+}
+
+setInterval(watchblog, 100);
